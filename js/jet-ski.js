8dc2d0fb6ca7a0cc66baf387831644c0f434a99d
Ravish Singh <ravishs@coastins.com.au>
2026-09-23 15:09:25 +0800

    Update jet-ski.js

--- js/jet-ski.js
+++ js/jet-ski.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let currentSectionNum = 1;    // maps to section id="step-N"
   let referralRequired = false;
 
-const DEV_BYPASS_VALIDATION = true;
+const DEV_BYPASS_VALIDATION = False;
 
   const quoteState = {
     importantInfo: {},
